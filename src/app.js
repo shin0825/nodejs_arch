fe001bd71e8ba8c54c0e467231e45bedb3ac087f
shin0825 <g_chow@hotmail.co.jp>
2021-08-24 23:45:39 +0900

    add access point 'api/get_music_profile'

--- src/app.js
+++ src/app.js
@@ -57,3 +57,46 @@ app.get('/get-music-profile', function (req, res) {
         });
     });
   });
+
+app.get('/api/get_music_profile', function (req, res) {
+    const client_id = process.env.CLIENT_ID;
+    const client_secret = process.env.SECRET;
+    const authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: { 'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64')) },
+      form: {
+        grant_type: 'client_credentials'
+      },
+      json: true
+    };
+    request.post(authOptions, function(error, response, body) {
+        if (error || response.statusCode != 200) {
+            return res.send(response);
+        }
+        const musicURL = req.query.id;
+        let ids, id;
+        let r = new RegExp('https://open.spotify.com/track/(.*)');
+        if ((ids = r.exec(musicURL)) === null) {
+            return res.json({name: 'NaN'});
+        }
+        id = ids[1];
+
+        const access_token = body.access_token;
+        const options = {
+            url: `https://api.spotify.com/v1/tracks/${id}`,
+            headers: { 'Authorization': 'Bearer ' + access_token },
+            json: true
+        };
+        request.get(options, function(error, response, body) {
+            if (error || response.statusCode != 200) {
+                return res.send("MISS!");
+            }
+            music_profile = {
+                name: body.name,
+                artist: body.artists[0].name,
+                url: musicURL
+            }
+            res.json(music_profile);
+        });
+    });
+  });
